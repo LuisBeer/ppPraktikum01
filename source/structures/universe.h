3175a9aca6122bac550b3bb1c0020bc3a4686a8e
wasserhannes <hannes00tromp@gmail.com>
2024-11-19 21:49:49 +0100

    Aufgabe 2 a) + b) + c) + d) + e) + f) done!

--- source/structures/universe.h
+++ source/structures/universe.h
@@ -6,7 +6,7 @@
 #include <cstdint>
 #include "structures/vector2d.h"
 #include "input_generator/input_generator.h"
-
+#include "bounding_box.h"
 
 class Universe {
     //Felder definieren
@@ -16,6 +16,23 @@ class Universe {
     Vector2d<double> forces;
     Vector2d<double> velocities;
     Vector2d<double> positions;
+
+    BoundingBox get_bounding_box() { //Methode gibt ein Objekt vom Typ Boundingbox zurück und nimmt keine Argumente an
+        double x_min = positions[0];
+        double x_max = positions[0];
+        double y_min = positions[1];
+        double y_max = positions[1];
+        // Durchlaufe alle Positionen und finde neue max Werte
+        for (const auto& pos : positions) {
+            x_min = std::min(x_min, pos[0]);
+            x_max = std::max(x_max, pos[0]);
+            y_min = std::min(y_min, pos[1]);
+            y_max = std::max(y_max, pos[1]);
+        }
+        //Erstelle BoundingBox mit entsprechenden Grenzen damit sie so klein wie möglich ist
+        return BoundingBox(x_min, x_max, y_min, y_max);
+    }
+
 public: //Standardkonstruktor erzeugt leeres Universum
     Universe()
         : num_bodies(0),
