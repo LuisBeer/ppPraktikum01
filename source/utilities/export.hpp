febc0ceabee20b74e7b4ae6b519caf15e5bc742d
Lukas Knöpfle <85690063+123-LP@users.noreply.github.com>
2024-11-25 10:31:37 +0100

    lezte compiler fehler

--- source/utilities/export.hpp
+++ source/utilities/export.hpp
@@ -5,51 +5,8 @@
 #include <structures/vector2d.h>
 #include <structures/universe.h>
 
-static void save_universe(const std::filesystem::path& save_universe_path, const Universe& universe) {
-    // Öffnen der Ausgabedatei
-    std::ofstream universe_file(save_universe_path);
-
-    if (!universe_file.is_open()) {
-        throw std::invalid_argument("Could not save universe to the given file!");
-    }
-
-    // Setzen der Präzision für double-Werte
-    universe_file << std::fixed << std::setprecision(6);
-
-    // Schreiben der Anzahl der Himmelskörper
-    universe_file << "### Bodies\n";
-    universe_file << universe.num_bodies << '\n';
-
-    // Schreiben der Positionen
-    universe_file << "### Positions\n";
-    for (const auto& position : universe.positions) {
-        universe_file << position[0] << " " << position[1] << '\n';
-    }
-
-    // Schreiben der Gewichte
-    universe_file << "### Weights\n";
-    for (const auto& weight : universe.weights) {
-        universe_file << weight << '\n';
-    }
-
-    // Schreiben der Geschwindigkeiten
-    universe_file << "### Velocities\n";
-    for (const auto& velocity : universe.velocities) {
-        universe_file << velocity[0] << " " << velocity[1] << '\n';
-    }
-
-    // Schreiben der Kräfte
-    universe_file << "### Forces\n";
-    for (const auto& force : universe.forces) {
-        universe_file << force[0] << " " << force[1] << '\n';
-    }
-
-    // Schließen der Datei
-    universe_file.close();
-}
 
 
-/*
 static void save_universe(const std::filesystem::path& save_path, const Universe& universe){
     std::ofstream universe_file(save_path);
     if(! universe_file.is_open()){
@@ -65,7 +22,7 @@ static void save_universe(const std::filesystem::path& save_path, const Universe
     // Speichern der Positionen
     universe_file << "###Positions\n";
     for (const auto& position : universe.positions) {
-        universe_file << position.x << " " << position.y <<"\n";
+        universe_file << position[0] << " " << position[1] <<"\n";
     }
     // Speichern der Gewichte
     universe_file << "###Weights\n";
@@ -75,14 +32,14 @@ static void save_universe(const std::filesystem::path& save_path, const Universe
     // Speichern der Geschwindigkeiten
     universe_file << "###Velocities\n";
     for (const auto& velocity : universe.velocities) {
-        universe_file << velocity.x << " " << velocity.y <<"\n";
+        universe_file << velocity[0] << " " << velocity[1] <<"\n";
     }
     // Speichern der Kräfte
     universe_file << "###Forces\n";
     for (const auto& force : universe.forces) {
-        universe_file << force.x << " " << force.y <<"\n";
+        universe_file << force[0] << " " << force[1] <<"\n";
     }
     universe_file.close();
 
 }
-*/
+
